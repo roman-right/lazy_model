83328ee375a1de6abd7d26c13171ac27b39a65c1
Roman <roman-right@protonmail.com>
2023-07-04 18:09:52 -0600

    fix | pydantic version checker | int -> str

--- lazy_model/main.py
+++ lazy_model/main.py
@@ -1,7 +1,7 @@
 import pydantic
 
 # check pydantic version
-if pydantic.version.VERSION.split(".")[0] == 1:
+if pydantic.version.VERSION.split(".")[0] == "1":
     from lazy_model.parser.old import LazyModel  # noqa: F401
 else:
     from lazy_model.parser.new import LazyModel  # noqa: F401
